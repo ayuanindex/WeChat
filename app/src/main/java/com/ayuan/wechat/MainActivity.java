c3028548ae4f2dd0beeec81f5002684aa95df0c2
lzy21 <lzy2118053626@hotmail.com>
2018-11-11 15:26:53 +0800

    change

--- app/src/main/java/com/ayuan/wechat/MainActivity.java
+++ app/src/main/java/com/ayuan/wechat/MainActivity.java
@@ -1,11 +1,9 @@
 package com.ayuan.wechat;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
